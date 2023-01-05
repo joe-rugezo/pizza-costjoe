5506c62ea8197d024111083e8e188678eba8cf1b
joe-rugezo <118993541+joe-rugezo@users.noreply.github.com>
2023-01-05 11:51:59 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+game.splash("let's calculate the pizza cost")
+let labor = game.askForNumber("what the labor")
+let rent = game.askForNumber("what the rent")
+let HST = game.askForNumber("what the HST")
+let materials = game.askForNumber("what the amount of materials")
+let pizza_cost = 0
